14f0ebcfb2fc11393ec701fdf111d768e31a84c9
kaldne <tseringkalden1964@gmail.com>
2020-10-06 12:25:41 -0700

    python n javascript

---
+++ .history/src/firebase_20201006122023.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDaLa5WxlOHS6MztM9IYl6uKHj0190Y4Wg',
+  authDomain: 'myporfolio-969cb.firebaseapp.com',
+  databaseURL: 'https://myporfolio-969cb.firebaseio.com',
+  projectId: 'myporfolio-969cb',
+  storageBucket: 'myporfolio-969cb.appspot.com',
+  messagingSenderId: '665873999203',
+  appId: '1:665873999203:web:f3167a21093db68fcd1f56',
+  measurementId: 'G-YC0MY91MHD',
+};
