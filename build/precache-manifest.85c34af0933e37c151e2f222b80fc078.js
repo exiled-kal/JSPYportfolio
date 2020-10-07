a2035d0a325acf70bc137f07f148668f5a7b0ded
kalden <tseringkalden1964@gmail.com>
2020-10-06 18:31:27 -0700

    done

---
+++ build/precache-manifest.85c34af0933e37c151e2f222b80fc078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08991984067cd4ced0a591887b41684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ee45a612e4ea441127",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ee45a612e4ea441127",
+    "url": "/static/js/main.d1d8591a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
